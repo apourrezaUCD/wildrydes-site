2b67fd2b8cd54aacf65300b0636a1331d3431fb1
apourrezaUCD <132355401+apourrezaUCD@users.noreply.github.com>
2025-03-17 16:08:02 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_UqfF6FwXR', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3pnoem4mou3ha92sj3n0pd6gnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        awsClientSecret: 'f0g902h5bogf7qlih5u6hsqll7akgonsf4hhsiu3msplddkcj65', // Keep this secure
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
