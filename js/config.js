6961e6569b30d2efed9f59cb6c5a1246c08fd80a
apourrezaUCD <132355401+apourrezaUCD@users.noreply.github.com>
2025-03-17 12:59:43 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_UqfF6FwXR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3pnoem4mou3ha92sj3n0pd6gnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
