caeac1879c19197bd1589d1c7485e0c81df24300
apourrezaUCD <132355401+apourrezaUCD@users.noreply.github.com>
2025-03-17 13:01:29 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_UqfF6FwXR', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3pnoem4mou3ha92sj3n0pd6gnv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
